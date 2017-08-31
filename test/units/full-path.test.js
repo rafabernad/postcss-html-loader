d858296a8a3e98e7400b197265aec529cebaca9f
LasaleFamine <info@godev.space>
2017-08-31 22:27:57 +0200

    Fixed full path test

--- test/units/full-path.test.js
+++ test/units/full-path.test.js
@@ -4,7 +4,7 @@ import fullPath from '../../src/lib/full-path';
 
 test('fullPath()', t => {
   t.is(
-    fullPath('test.postcss', '/my/dir/file.html'),
+    fullPath('test.postcss', 'my/dir/file.html'),
     join('my', 'dir', 'test.postcss'),
     'should return the full path of a source based on the path of the resource file'
   );
